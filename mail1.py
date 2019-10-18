5ffa06b1b9fa5b395f60ef99188998f47431b196
abhishek8075374519 <36701698+abhishek8075374519@users.noreply.github.com>
2019-10-18 12:46:04 +0530

    Update mail1.py

--- mail1.py
+++ mail1.py
@@ -7,7 +7,8 @@ recipients.append(input("Enter recipient email id: "))
 message = input("Enter your message: ")
 while True:
     print('''press 1 to add another recipient
-    press 0 to send email''')
+press 0 to send email
+    ''')
     a = int(input("Enter your choice: "))
     if a == 0:
         break
